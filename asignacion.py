2eb178cdfac716c6b344703e7f612944d3b8ec7a
josuewil-source <menjivarjosuewilfredo@gmail.com>
2026-05-20 15:57:13 -0600

    asignacion

---
+++ asignacion.py
@@ -0,0 +1,16 @@
+x,y,z = "mandarina", "marañon", "mango"
+print(x)
+print(y)
+print(z)
+print("")
+x=y=z = "melon"
+print(x)
+print(y)
+print(z)
+print("")
+fruta = ["mandarina", "marañon", "mango", "kiwi"]
+x,y,z, a = fruta
+print(x)
+print(y)
+print(z)
+print(a)
