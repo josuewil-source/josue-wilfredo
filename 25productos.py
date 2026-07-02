7aa154a66e3d7f22e260be6cdf3024c337409c79
josuewil-source <menjivarjosuewilfredo@gmail.com>
2026-07-01 21:55:20 -0600

    Create 25productos.py

---
+++ 25productos.py
@@ -0,0 +1,7 @@
+productos = {
+    "Laptop": (850, ("Tecnología", "Computadoras")),
+    "Mouse": (25, ("Tecnología", "Accesorios")),
+    "Teclado": (40, ("Tecnología", "Accesorios"))
+}
+
+print(productos)
