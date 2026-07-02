c862943f903703eabe879f907581b4f71c0844c4
josuewil-source <menjivarjosuewilfredo@gmail.com>
2026-07-01 21:32:20 -0600

    Create calculadorasimple4.py

---
+++ calculadorasimple4.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+operador = "+"
+
+if operador == "+":
+    print(num1 + num2)
+elif operador == "-":
+    print(num1 - num2)
+elif operador == "*":
+    print(num1 * num2)
+elif operador == "/":
+    print(num1 / num2)
+else:
+    print("Operador no válido")
