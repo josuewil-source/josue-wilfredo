df9a68e46c17c56d33e89d810160520ce0bd4b54
josuewil-source <menjivarjosuewilfredo@gmail.com>
2026-07-01 21:33:38 -0600

    Create triangulo_valido5.py

---
+++ triangulo_valido5.py
@@ -0,0 +1,8 @@
+lado1 = 3
+lado2 = 4
+lado3 = 5
+
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
+    print("Sí forman un triángulo")
+else:
+    print("No forman un triángulo")
