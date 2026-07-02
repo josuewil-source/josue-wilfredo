e40cb83189af83dd422f1fd33949cf698c9ffec0
josuewil-source <menjivarjosuewilfredo@gmail.com>
2026-07-01 21:47:27 -0600

    Create estadisticasdedupla17.py

---
+++ estadisticasdedupla17.py
@@ -0,0 +1,9 @@
+numeros = (10, 20, 30, 40, 50)
+
+suma = sum(numeros)
+promedio = suma / len(numeros)
+mayor = max(numeros)
+
+print("Suma:", suma)
+print("Promedio:", promedio)
+print("Mayor:", mayor)
