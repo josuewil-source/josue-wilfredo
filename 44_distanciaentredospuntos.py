bcad1ae584db6f19e362e4f6f43e32e3b8f3e2a5
josuewil-source <menjivarjosuewilfredo@gmail.com>
2026-07-01 22:38:29 -0600

    Create 44_distanciaentredospuntos.py

---
+++ 44_distanciaentredospuntos.py
@@ -0,0 +1,12 @@
+# Importar librería matemática
+import math
+
+# Coordenadas
+punto1 = (0, 0)
+punto2 = (3, 4)
+
+# Calcular distancia
+distancia = math.sqrt((punto2[0]-punto1[0])**2 + (punto2[1]-punto1[1])**2)
+
+# Mostrar resultado
+print("Distancia:", distancia)
